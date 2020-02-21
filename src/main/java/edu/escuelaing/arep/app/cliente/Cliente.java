7f7721b88fe5540f32a93739ec0e497f12ce468c
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2020-02-21 12:27:36 -0500

    Iniciacion proyecto

---
+++ src/main/java/edu/escuelaing/arep/app/cliente/Cliente.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arep.app.cliente;
+
+import java.io.*; 
+import java.net.*; 
+
+
+public class Cliente { 
+  public static void main(String[] args) throws Exception { 
+      URL url = new URL(args[0]); 
+      try (BufferedReader reader = new BufferedReader(
+          new InputStreamReader(url.openStream()))) { 
+            String inputLine = null; 
+            while ((inputLine = reader.readLine()) != null) { 
+                  System.out.println(inputLine); 
+             } 
+       } catch (IOException x) { 
+               System.err.println(x); 
+       } 
+    } 
+}
